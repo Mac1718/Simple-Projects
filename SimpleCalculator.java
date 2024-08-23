ef53a2d3eb68e6a898af305c9a955e01296dea1b
Mahant <146162280+Mac1718@users.noreply.github.com>
2024-08-23 21:50:30 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,42 @@
+/*package Practice;
+import java.util.Scanner;
+
+public class SimpleCalculator 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        char d=sc.next().charAt(0);
+        switch(d)
+        {
+        case '+':
+        {
+            System.out.println(a+b);
+            break;
+        }
+        case '-':
+        {
+            System.out.println(a-b);
+            break;
+        }
+        case'*':
+        {
+            System.out.println(a*b);
+            break;
+        }
+        case'/':
+        {
+            System.out.println(a/b);
+            break;
+        }
+        case'%':
+        {
+            System.out.println(a%b);
+            break;
+        }
+        }
+    }
+}*/    
+
